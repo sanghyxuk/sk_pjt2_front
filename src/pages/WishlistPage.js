0d0718c6d93c9c6a6473228b4722bebf2a41347c
Songbomin <bmsong00@naver.com>
2025-02-13 17:44:56 +0900

    불편한 다른 박스 위치 해결

--- src/pages/WishlistPage.js
+++ src/pages/WishlistPage.js
@@ -19,57 +19,65 @@ function WishListPage() {
 
     return (
         <div className="outer-container">
-            <div className="wishlist-container">
-                <div className="wishlist-content">
-                    {/* 사이드바 영역 */}
+            <div className="main-content">
+                <div className="container">
+                    {/* 좌측 메뉴 영역 */}
                     <div className="sidebar-container">
                         <div className="sidebar">
                             <h3 className="sidebar-title">Manage My Account</h3>
                             <ul className="sidebar-menu">
                                 <li className="menu-item">
-                                    <Link to="/profile/edit" className="sidebar-link">Edit My Profile</Link>
+                                    <Link to="/profile/edit" className="sidebar-link">
+                                        Edit My Profile
+                                    </Link>
                                 </li>
                             </ul>
-
                             <h3 className="sidebar-title">My Items</h3>
                             <ul className="sidebar-menu">
                                 <li className="menu-item">
-                                    <Link to="/mysale" className="sidebar-link">My sale</Link>
+                                    <Link to="/mysale" className="sidebar-link">
+                                        My sale
+                                    </Link>
                                 </li>
                                 <li className="menu-item">
-                                    <Link to="/mypurchase" className="sidebar-link">My purchase</Link>
+                                    <Link to="/mypurchase" className="sidebar-link">
+                                        My purchase
+                                    </Link>
                                 </li>
                             </ul>
-
                             <h3 className="sidebar-title">My WishList</h3>
                             <ul className="sidebar-menu">
                                 <li className="menu-item active">
-                                    <Link to="/wishlist" className="sidebar-link">My WishList</Link>
+                                    <Link to="/wishlist" className="sidebar-link">
+                                        My WishList
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
                     </div>
 
-                    {/* 메인 콘텐츠 영역 */}
-                    <div className="wishlist-main">
-                        <h1 className="page-title">My WishList</h1>
-                        {wishlist.length > 0 ? (
-                            <div className="item-grid">
-                                {wishlist.map((item) => (
-                                    <div className="item-card" key={item.id}>
-                                        {item.discount && (
-                                            <div className="discount">-{item.discount}%</div>
-                                        )}
-                                        <img src={item.imageUrl} alt={item.title}/>
-                                        <h3>{item.title}</h3>
-                                        <p className="price">${item.price}</p>
-                                        <button className="add-to-cart-btn">Add To Cart</button>
-                                    </div>
-                                ))}
-                            </div>
-                        ) : (
-                            <p className="empty-message">찜한 상품이 없습니다.</p>
-                        )}
+                    {/* 우측 메인 영역 */}
+                    <div className="profile-container">
+                        <div className="main-area">
+                            <h1 className="page-title">My WishList</h1>
+                            {wishlist.length > 0 ? (
+                                <div className="item-grid">
+                                    {wishlist.map((item) => (
+                                        <div className="item-card" key={item.id}>
+                                            {item.discount && (
+                                                <div className="discount">-{item.discount}%</div>
+                                            )}
+                                            <img src={item.imageUrl} alt={item.title} />
+                                            <h3>{item.title}</h3>
+                                            <p className="price">${item.price}</p>
+                                            <button className="add-to-cart-btn">Add To Cart</button>
+                                        </div>
+                                    ))}
+                                </div>
+                            ) : (
+                                <p className="empty-message">찜한 상품이 없습니다.</p>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
