2daa30df4c301a3fb16c30c17289474f27b4b983
ukjinSPACE <dnrwls128@gmail.com>
2025-02-14 17:04:18 +0900

    판매목록api 1

--- src/api/mysaleApi.js
+++ src/api/mysaleApi.js
@@ -1,11 +1,18 @@
-import axios from "axios";
+//import axios from "axios";
+import api from "./axios";
+//const BASE_URL = "http://localhost:8080/mypurchase"; // 백엔드 API 주소
 
-const BASE_URL = "http://localhost:8080/mysale"; // 백엔드 API 주소
-
-// 내가 판매한 물품 목록 가져오기
-export const getMySaleItems = async (userId) => {
+export const getMySaleItems = async (page = 0, size = 10, email) => {
     try {
-        const response = await axios.get(`${BASE_URL}/${userId}`);
+        const params = new URLSearchParams();
+        params.append('page', page);
+        params.append('size', size);
+
+        const response = await api.get(`/myPage//mySale?page=${page}&size=${size}`, {
+            headers: {
+                "X-Auth-User": email
+            }
+        });
         return response.data;
     } catch (error) {
         console.error("판매한 물품을 불러오는 중 오류 발생:", error);
